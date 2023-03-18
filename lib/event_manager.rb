9642b6bb00fc4eb3ae2529758efc328283a66373
VAnto <vntantonio@gmail.com>
2023-03-18 16:01:25 +0100

    Initialize lib/event_manager.rb

---
+++ lib/event_manager.rb
@@ -0,0 +1,15 @@
+if File.exists?('event_attendees.csv')
+  puts "I exist!"
+  puts "------"
+end
+
+#contents = File.read('event_attendees.csv')
+#puts contents
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
