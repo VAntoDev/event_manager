bfd82354c09a634bbada8d77585884a1f9ec3de4
VAnto <vntantonio@gmail.com>
2023-03-19 02:49:26 +0100

    Use of civicinfo_v2 API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,29 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]  #to_s makes the nil become "", rjust adjusts strings smaller than 5 with '0', slice makes longer strings only 5 digits
 end
 
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  
+    legislator_names = legislators.map(&:name)
+  
+    legislators_string = legislator_names.join(", ")
+    rescue
+      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+end
 contents = CSV.open(
   'event_attendees.csv',     #filename
   headers: true,             #find headers in the file
@@ -14,7 +34,9 @@ contents.each do |row|
   name = row[:first_name]    #picks each name in the header first_name
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "Name -> #{name} - #{zipcode} <- Zipcode"
+  legislators = legislators_by_zipcode(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
 end
+
 
 
