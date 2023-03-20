27821e14ba7fd0314fa491d213aec27abae65b25
VAnto <vntantonio@gmail.com>
2023-03-20 17:24:20 +0100

    Add phone number on form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,22 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]  #to_s makes the nil become "", rjust adjusts strings smaller than 5 with '0', slice makes longer strings only 5 digits
 end
 
+def clean_phone_number(phone_number)
+  phone_number.gsub!(/[^0-9A-Za-z]/, '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11
+    if phone_number[0] == 1
+      phone_number.chr
+      phone_number
+    else
+      "Not Valid Phone Number"
+    end
+  else
+    "Not Valid Phone Number"
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -44,10 +60,14 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]    #picks each name in the header first_name
   zipcode = clean_zipcode(row[:zipcode])
+  phone_num = clean_phone_number(row[:homephone])
+
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
+
 end
+
